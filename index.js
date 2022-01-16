e432d52462bc76d5386f5ddc7acfe6b2336bd85c
suasua <tndk_19@naver.com>
2022-01-16 20:45:32 +0900

    node.js 2차시
    - db config 분리

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
 
-//{useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify: false}
+const confing = require("./config/key");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://sua:sua_010!@plate-cluster0.bo64x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-,).then(() => console.log('mongoDB Connected..'))
+mongoose.connect(confing.mongoURI).then(() => console.log('mongoDB Connected..'))
 .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('hello'))
+app.get('/', (req, res) => res.send('hello ~~^^'))
+
+app.post('/register', (req, res) => {
+
+
+    // 클라이언트에게 받은 정보를 데이터베이스에 넣는다
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({success: true})
+    });
+
+})
+
 
 app.listen(port,() => console.log('example app on port ${port}'))
